03579497ccb32497d71e844ab1530002ef2e3296
Raghuveer singh <singraghuveer@gmail.com>
2016-03-08 19:28:49 +0530

    new1

--- healthok/app/src/main/java/com/example/zues/healthok/HomePage.java
+++ healthok/app/src/main/java/com/example/zues/healthok/HomePage.java
@@ -1,16 +1,11 @@
 package com.example.zues.healthok;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
-import com.example.zues.healthok.R;
 
 /**
  * Created by Ashu on 2/28/2016.
@@ -18,11 +13,12 @@ import com.example.zues.healthok.R;
 public class HomePage extends Activity {
 
     Button b;
+    ImageView im;
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
-        ImageView im=(ImageView)findViewById(R.id.imageView12);
+        im=(ImageView)findViewById(R.id.imageView12);
         im.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {

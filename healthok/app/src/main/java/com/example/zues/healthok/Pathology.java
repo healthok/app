0ef11d174a3012674e03f9fa95e81b5fa2feeb2b
ashwani0187 <ashwinmmmec794@gmail.com>
2016-03-10 21:00:04 -0800

    NewLatestCode

---
+++ healthok/app/src/main/java/com/example/zues/healthok/Pathology.java
@@ -0,0 +1,19 @@
+package com.example.zues.healthok;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Ashu on 3/9/2016.
+ */
+public class Pathology extends Activity {
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.pathology);
+
+
+}
+
+}

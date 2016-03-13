111f1c2a3023a4bb46e9cf66f2e0faa81fbb19d4
AbhayHealthOK <abhay-jaiswal@health-ok.in>
2016-03-14 00:26:55 +0530

    revert accidental synch by Vaibhav
    revert accidental synch by Vaibhav

--- final my_profile1/js/customers.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-   $scope.name = "joy";
-    $scope.email = "joy@gmail.com";
-	 $scope.mobile = "9876543210";
-	  $scope.address = "gorakhpur";
-  
-});

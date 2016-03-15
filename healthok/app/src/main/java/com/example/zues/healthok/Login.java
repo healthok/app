4b228491f1ef17b324765ee06f11867556eb67de
Raghuveer singh <singraghuveer@gmail.com>
2016-03-15 10:25:38 +0530

    hospitadata

--- healthok/app/src/main/java/com/example/zues/healthok/Login.java
+++ healthok/app/src/main/java/com/example/zues/healthok/Login.java
@@ -14,8 +14,6 @@ import android.widget.Toast;
 
 import com.example.zues.healthok.util.ServiceHandler;
 
-import java.lang.String;
-
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;

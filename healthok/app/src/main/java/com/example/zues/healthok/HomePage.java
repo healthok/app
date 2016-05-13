7f8d7b006a019fab54eea7e620b26523f75b48b6
Raghuveer singh <singraghuveer@gmail.com>
2016-05-13 19:03:08 +0530

    menuitem

--- healthok/app/src/main/java/com/example/zues/healthok/HomePage.java
+++ healthok/app/src/main/java/com/example/zues/healthok/HomePage.java
@@ -1,16 +1,22 @@
 package com.example.zues.healthok;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
 /**
  * Created by Ashu on 2/28/2016.
  */
 public class HomePage extends Activity {
+
 
     Button b;
     ImageView im;
@@ -27,7 +33,7 @@ public class HomePage extends Activity {
                 setContentView(R.layout.medicine1);
             }
         });
- b=(Button)findViewById(R.id.button4);
+        b=(Button)findViewById(R.id.button4);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,5 +43,19 @@ public class HomePage extends Activity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_home_page, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                         (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        return super.onCreateOptionsMenu(menu);
     }
 }

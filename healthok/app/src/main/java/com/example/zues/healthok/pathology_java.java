a70f970b023e2dbbcde618b8a74bdb8b8475c0dd
AbhayHealthOK <abhay-jaiswal@health-ok.in>
2016-05-19 12:48:10 +0530

    fixed compilation error

--- healthok/app/src/main/java/com/example/zues/healthok/pathology_java.java
+++ healthok/app/src/main/java/com/example/zues/healthok/pathology_java.java
@@ -11,7 +11,7 @@ public class pathology_java extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.HomePage);
+      //  setContentView(R.layout.HomePage);
     }
 
 
